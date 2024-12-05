a9d74c311893fb5781f326c11a48956e672fdbd4
Keepgoingcr007 <keepgoingcr7@gmail.com>
2024-12-04 21:18:57 -0600

    update

--- provider.tf
+++ provider.tf
@@ -1,11 +1,3 @@
-
-
-
-
-
-
-
-
 terraform {
   required_providers {
     aws = {
@@ -13,20 +5,18 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket         = "my-terraform-state"
+    key            = "terraform/state"
+    region         = "us-east-1"
+    # encrypt        = true
+    # dynamodb_table = "terraform-locks"
+  }
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
-
-
-
-
-
-
-
-
 
 resource "aws_s3_bucket" "example" {
   bucket = "my-tf-test-bucket"
