088b0baeb12af3c0622a6dc124493d892fd61e96
Keepgoingcr007 <keepgoingcr7@gmail.com>
2024-12-04 21:51:45 -0600

    ufi

--- terraform-infra/provider.tf
+++ terraform-infra/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "my-terraform-state"
+    bucket = "my-terraform-statedlkgfsdhpoagih"
     key    = "terraform/state"
     region = "us-east-1"
   }
